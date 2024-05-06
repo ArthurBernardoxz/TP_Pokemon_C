faff434d00c8571761e14ad5d3ad47637b064683
Arthur Bernardo <86016100+ArthurBernardoxz@users.noreply.github.com>
2024-05-06 20:14:03 -0300

    init

--- Pokemon.c
+++ Pokemon.c
@@ -3,6 +3,24 @@
 #include <stdio.h>
 
 int main() {
-    
-    return 0;
+   /*int N_pokemons_P1, N_pokemons_P2;*/
+   typedef struct Pokemon_P1 {
+      char nome;
+      int ataque;
+      int defesa;
+      int vida;
+      char tipo;
+   };
+  /* typedef struct Pokemon_P2 {
+      char nome;
+      int ataque;
+      int defesa;
+      int vida;
+      char tipo;
+   };*/
+   struct Pokemon_P1 P1;
+   /*struct Pokemon_P2 P2;*/
+   scanf("%s %d %d %d %s", P1.nome,&P1.ataque,&P1.defesa,&P1.vida,P1.tipo);   
+   printf("%s %d %d %d %s", P1.nome,P1.ataque,P1.defesa,P1.vida,P1.tipo);   
+   return 0;
 }
